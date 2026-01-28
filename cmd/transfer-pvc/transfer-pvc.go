2628f235b35d21d49d8743061caa567add1b42b6
Marek Aufart <maufart@redhat.com>
2026-01-28 14:19:26 +0100

    Update comment transfer PVC
    Signed-off-by: Marek Aufart <maufart@redhat.com>

--- cmd/transfer-pvc/transfer-pvc.go
+++ cmd/transfer-pvc/transfer-pvc.go
@@ -386,8 +386,7 @@ func (t *TransferPVCCommand) run() error {
 			Namespace: srcPVC.Namespace,
 		}, e.Hostname(), e.IngressPort(), &transport.Options{
 			Labels: labels,
-			// Coderabbit suggest SourceImage, keep it for later once we know this better
-			Image:  t.Flags.DestinationImage,
+			Image:  t.Flags.DestinationImage,	// Coderabbit suggest SourceImage, keep it for later once we know this better
 		},
 	)
 	if err != nil {
